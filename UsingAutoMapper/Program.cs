a8d40cbf2e1c7fdbc12163eabe94ba5cddb92277
Andre Santos <63032209+Andre834@users.noreply.github.com>
2023-04-02 00:51:38 -0300

    Adicionar arquivos de projeto.

---
+++ UsingAutoMapper/Program.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using System;
+
+namespace UsingAutoMapper
+{
+    /*
+     *Como sabemos, se o nome da propriedade de dados for diferente para as classes de origem e destino,
+     *o AutoMapper em C# não poderá mapear essas propriedades por padrão. No entanto, 
+     *usando a opção ForMember, podemos mapear entre as propriedades de nomes diferentes.
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new MapperConfiguration(cfg =>
+
+             cfg.CreateMap<Student, StudentDT>()
+               .ForMember(dest => dest.Fullname, act => act.MapFrom(src => src.Name))// permite mapear propriedades com nomes diferentes
+
+            );
+
+            Student stu = new Student
+
+            {
+
+                Name = "ShivamFullName",
+
+                Age = 20,
+
+                Address = "Bihar",
+
+                Department = "IT"
+
+            };
+
+
+            //Using AutoMapper
+
+            var mapper = new Mapper(config);
+
+            var stuDT = mapper.Map<StudentDT>(stu);
+
+            //OR another way of initializing AutoMapper.
+
+            //var stuDT2 = mapper.Map<Student, StudentDT>(stu);
+
+            //on running this , it will give us the output as expected
+
+            Console.WriteLine("Name:" + stuDT.Fullname + ", Age:" + stuDT.Age + ", Address:" + stuDT.Address + ", Department:" + stuDT.Department);
+
+            Console.ReadLine();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ UsingAutoMapper/StudentDT.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingAutoMapper
+{
+    public class StudentDT
+
+    {
+
+        public string Fullname { get; set; }
+
+        public int Age { get; set; }
+
+        public string Address { get; set; }
+
+        public string Department { get; set; }
+
+    }
+}
